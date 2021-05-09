3fd89d139f2eea8563471ced24f46502c65ad435
Sary Jamie <sary.jamie@gmail.com>
2021-05-09 05:40:05 -0700

    creating a new component folder and a component file use-counter to pass a function of useCounter

---
+++ src/components/hooks/use-counter.js
@@ -0,0 +1,15 @@
+import { useState, useCounter } from "react";
+
+// using the keyword "use" is a MUST in order to use react hooks
+const useCounter = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+};
+export default useCounter;
