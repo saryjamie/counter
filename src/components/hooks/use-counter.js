994a31ebbf6980dc60ab01070099ac3740b9ef0c
Sary Jamie <sary.jamie@gmail.com>
2021-05-20 05:41:08 -0700

    passing counterUdateFn in the useCounter variable and pass it to the setState in the use-counter.js file

--- src/components/hooks/use-counter.js
+++ src/components/hooks/use-counter.js
@@ -2,12 +2,12 @@ import React, { useState, useCounter } from "react";
 
 // in order to use React Hooks we should always stat with -use- keyword
 
-const useCounter = () => {
+const useCounter = (counterUpdateFn) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      setCounter(counterUpdateFn);
     }, 1000);
 
     return () => clearInterval(interval);
