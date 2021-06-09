2ac6e829e17b106c800ef4723de1c3638cc259e5
Sary Jamie <sary.jamie@gmail.com>
2021-06-09 15:15:38 -0700

    fixing a bug in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import BackwardCounter from "./components/BackwardCounter";
 import ForwardCounter from "./components/ForwardCounter";
 
