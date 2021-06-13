364018c524622f9147e5bec039dc31b1faafbc5f
Sary Jamie <sary.jamie@gmail.com>
2021-06-13 00:39:26 -0700

    cleaning ForwardCounter code

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -6,6 +6,5 @@ import useCounter from "../hooks/use-counter";
 const ForwardCounter = () => {
   const counter = useCounter();
   return <Card>{counter}</Card>;
-  console.log("testing...");
 };
 export default ForwardCounter;
